101ee5a3cb5cfd18a6bb9f8641710348f5412d92
João Soares <jpcirqueira81@gmail.com>
2020-04-28 12:16:20 -0300

    feito mock para api

--- src/components/elementsaga.test.js
+++ src/components/elementsaga.test.js
@@ -1,6 +1,10 @@
 import { runSaga } from 'redux-saga'
 import { getElement } from '../store/modules/element/sagas'
-import { getElementSuccess } from '../store/modules/element/actions'
+import { getElementSuccess, getElementFailure } from '../store/modules/element/actions'
+import MockAdapter from 'axios-mock-adapter'
+import api from '../service/api'
+
+const apiMock = new MockAdapter(api)
 
 describe('teste saga', () => {
     
@@ -8,8 +12,16 @@ describe('teste saga', () => {
     
     it('saga deve funcionar sem falhas', async () => {
         
+        apiMock.onGet('element').reply(200, ['teste'])
         await runSaga({ dispatch }, getElement).toPromise()
 
         expect(dispatch).toHaveBeenCalledWith(getElementSuccess(['teste']))
+    })
+
+    it('saga deve disparar error', async () => {
+        
+        apiMock.onGet('element').reply(500)
+        await runSaga({ dispatch }, getElement).toPromise()
+        expect(dispatch).toHaveBeenCalledWith(getElementFailure())
     })
 })
