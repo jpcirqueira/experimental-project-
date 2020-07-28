1deef88acb1df5bb26dca114bc53940e67569fcc
João Soares <jpcirqueira81@gmail.com>
2020-07-28 19:48:13 -0300

    add rich text with Quill.

---
+++ src/components/testQuill/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { htmlEditButton } from "./quill.htmlEditButton.js";
+import Quill from  'quill';
+export default function QuillRichText() {
+    var editor = new Quill('.editor');
+
+    return(
+        <>
+            
+        </>
+    );
+}   
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 
 import AddList from './components/addList';
 import TestFormik from '../src/components/testformik/index';
+import QuillRichText from '../src/components/testQuill/index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TestFormik />
+    <QuillRichText />
   </React.StrictMode>,
   document.getElementById('root')
 );
